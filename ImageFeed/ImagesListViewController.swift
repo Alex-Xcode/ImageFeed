b19bc8092c1437df07689b98a500d3ac8fe7896c
Alex-Xcode <alex.ocode@gmail.com>
2025-01-10 18:43:53 +0500

    Added new feature

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -2,21 +2,21 @@ import UIKit
 
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
-    
+
     private let photosName = (0..<20).map { "\($0)" }
-    
+    private let currentDate = Date()
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .none
         return formatter
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
     }
-    
+
     private func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -30,7 +30,7 @@ extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photosName.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: ImagesListCell.reuseIdentifier,
@@ -38,7 +38,7 @@ extension ImagesListViewController: UITableViewDataSource {
         ) as? ImagesListCell else {
             fatalError("Unable to dequeue ImagesListCell")
         }
-        
+
         configure(cell, at: indexPath)
         return cell
     }
@@ -47,10 +47,10 @@ extension ImagesListViewController: UITableViewDataSource {
 extension ImagesListViewController {
     private func configure(_ cell: ImagesListCell, at indexPath: IndexPath) {
         let imageName = photosName[indexPath.row]
-        let dateText = dateFormatter.string(from: Date())
+        let dateText = dateFormatter.string(from: currentDate) // Используем предсозданную дату
         let isLiked = indexPath.row % 2 == 0
         let likeImageName = isLiked ? "like_button_on" : "like_button_off"
-        
+
         cell.cellImage.image = UIImage(named: imageName)
         cell.dateLabel.text = dateText
         cell.likeButton.setImage(UIImage(named: likeImageName), for: .normal)
@@ -60,7 +60,7 @@ extension ImagesListViewController {
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else { return 200 }
-        
+
         let padding: CGFloat = 16
         let availableWidth = tableView.bounds.width - padding
         let imageAspectRatio = image.size.height / image.size.width
